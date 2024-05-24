2705ebd02ecdca305f2b1916a1558542ed355f75
adriendejean <167533680+adriendejean@users.noreply.github.com>
2024-05-24 19:18:00 +0200

    Revert "revert"
    This reverts commit 6c9b5bc46c97112af53a093e459e318a360d5cfa.

--- src/map/HoverBubble.jsx
+++ src/map/HoverBubble.jsx
@@ -21,7 +21,7 @@ const Hover = props => {
       className="p6o-map-hover"
       style={style}>
       {colocated > 0 ?
-        <>{node.properties.title} <span className="p6o-map-hover-colocated">+ {colocated} more</span></> : <>{node.properties.title}</> 
+        <>{node.title} <span className="p6o-map-hover-colocated">+ {colocated} more</span></> : <>{node.title}</> 
       }
       {node.geometry?.granularity &&
         <div className="p6o-map-hover-granularity">
